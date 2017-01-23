c886cd6b9f31045fa99648062ec82434693d41a2
ifwelove <a5963745@gmail.com>
2017-01-24 00:01:22 +0800

    add xml namespace

--- src/Adapter/FacebookAdapter.php
+++ src/Adapter/FacebookAdapter.php
@@ -14,30 +14,31 @@ class FacebookAdapter extends AbstractAdapter
 
     public function toXml()
     {
+        $namespace = 'http://base.google.com/ns/1.0';
         $array = $this->processed['success'];
-        $xml = new \SimpleXMLElement('<feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="http://base.google.com/ns/1.0"/>');
+        $xml = new \SimpleXMLElement(sprintf('<feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="%s"/>', $namespace));
         if (isset($this->config['title'])) {
             $xml->addChild('title', $this->addCDATA($this->config['title']));
         }
         if (isset($this->config['link'])) {
             $link = $xml->addChild('link');
             $link->addAttribute('rel', 'self');
-            $link->addAttribute('href', $this->addCDATA($this->config['link']));
+            $link->addAttribute('href', $this->config['link']);
         }
         foreach ($array as $item) {
             $entry = $xml->addChild('entry');
-            $entry->addChild('g:title', $this->addCDATA($item['title']));
-            $entry->addChild('g:id', $item['id']);
-            $entry->addChild('g:description', $this->addCDATA($item['description']));
-            $entry->addChild('g:link', $this->addCDATA($item['link']));
-            $entry->addChild('g:image_link', $this->addCDATA($item['image_link']));
-            $entry->addChild('g:brand', $this->addCDATA($item['brand']));
-            $entry->addChild('g:condition', $item['condition']);
-            $entry->addChild('g:availability', $item['availability']);
-            $entry->addChild('g:price', $item['price']);
-            $entry->addChild('g:product_type', $this->addCDATA($item['product_type']));
-            $entry->addChild('g:google_product_category', $this->addCDATA($item['google_product_category']));
-            $entry->addChild('g:custom_label_0', $this->addCDATA($item['custom_label_0']));
+            $entry->addChild('title', $this->addCDATA($item['title']), $namespace);
+            $entry->addChild('id', $item['id'], $namespace);
+            $entry->addChild('description', $this->addCDATA($item['description']), $namespace);
+            $entry->addChild('link', $this->addCDATA($item['link']), $namespace);
+            $entry->addChild('image_link', $this->addCDATA($item['image_link']), $namespace);
+            $entry->addChild('brand', $this->addCDATA($item['brand']), $namespace);
+            $entry->addChild('condition', $item['condition'], $namespace);
+            $entry->addChild('availability', $item['availability'], $namespace);
+            $entry->addChild('price', $item['price'], $namespace);
+            $entry->addChild('product_type', $this->addCDATA($item['product_type']), $namespace);
+            $entry->addChild('google_product_category', $this->addCDATA($item['google_product_category']), $namespace);
+            $entry->addChild('custom_label_0', $this->addCDATA($item['custom_label_0']), $namespace);
             
 //            $shipping = $entry->addChild('shipping');
 //            $shipping->addChild('country', $item['shipping']['country']);
